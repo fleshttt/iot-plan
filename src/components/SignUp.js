7e39b8d1ec2bff3f380d63955674ef6630f04d77
Khirov Victor <fleshoris@gmail.com>
2024-12-08 11:00:38 +0700

    0.3

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Login.css';  // Подключаем файл стилей
+import './Login/Login.css';  // Подключаем файл стилей
 
 function SignUp() {
   return (
